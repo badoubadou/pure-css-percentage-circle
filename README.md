72897552551d57e443a96cb23a57b18f03ce8e5a
Romain Malauzat <rmalauzat@gmail.com>
2017-08-15 12:54:35 +0200

    first

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Getting Started With Gulp
 
-Gulp example app for the tutorial
+Description:
+
+A pure CSS progress bar that shows progress bar with percentage in a circular bar view, inspired by André Firchow. I translated to stylus, added some animation and used data atribute for the percentage.
+
+How to use it:
+	Load the cpbs.css in the document’s head section.
+
+Had the html and set the data atribute "p" to your wanted percentage
+	<div class="c100" data-p="70">
+		<div class="slice">
+			<div class="second_half"></div>
+			<div class="warp_first_half">
+				<div class="first_half"></div>
+			</div>
+		</div>
+	</div>
+
+check it live here
+	https://codepen.io/romainmalauzat/pen/XaKjVM
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,31 @@
 // Gulpfile
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var minifyCSS    = require('gulp-minify-css');
+var stylus    = require('gulp-stylus');
+var autoprefixer = require('gulp-autoprefixer');
+var pug = require('gulp-pug');
+ 
+
+
+gulp.task('default', function() {
+    //implementation of the task
+    gutil.log('== My Log Task ==');
+    gulp.src('./styles/*.styl')
+	.pipe(stylus({
+		compress: true
+	}))
+	.pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+	.pipe(minifyCSS())
+	.pipe(gulp.dest('./styles/'))
+
+	gulp.src('./pug/*.pug')
+	.pipe(pug({
+	// Your options in here. 
+	}))
+	.pipe(gulp.dest('./'))
+
+});
